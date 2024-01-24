0949f9c7bca33e665af3f001cf47e424c35c1f13
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-01-24 23:57:31 +0530

    Create Pen Object

--- Practice/Example.java
+++ Practice/Example.java
@@ -1,8 +1,20 @@
 package Oops.Practice;
 
+class Pen {
+    String color;
+    String type;
+
+    public void write() {
+        System.out.println("writing something");
+    }
+}
 public class Example {
     public static void main(String[] args) {
-        
+        Pen pen1=new Pen();
+        pen1.color="blue";
+        pen1.type="gel";
+
+        pen1.write();
     }
     
 }
