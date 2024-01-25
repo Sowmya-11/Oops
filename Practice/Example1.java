123e60ce21489c8d74e40400a297f1b2665b15b8
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-01-25 22:59:37 +0530

    Modified age

--- Practice/Example1.java
+++ Practice/Example1.java
@@ -13,7 +13,7 @@ public class Example1 {
     public static void main(String[] args) {
         Student s1=new Student();
         s1.name="Dora";
-        s1.age=16;
+        s1.age=17;
 
         s1.printInfo();
     }
