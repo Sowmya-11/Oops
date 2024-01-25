3a7b111121e7a94c842a9b12be9c1698ddde7b26
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-01-25 23:38:02 +0530

    Method OverLoading Example

---
+++ Practice/MethodOverLoading.java
@@ -0,0 +1,27 @@
+package Oops.Practice;
+
+class Sum {
+
+    public int printSum(int a, int b) {
+        return a+b;
+    }
+
+    public int printSum(int a, int b,int c) {
+        return a+b+c;
+    }
+
+    public float printSum(float a,float b) {
+        return a+b;
+    }
+}
+
+public class MethodOverLoading {
+    
+    public static void main(String[] args) {
+        Sum s=new Sum();
+        System.out.println(s.printSum(20,20)); 
+        System.out.println(s.printSum(20,20,20)); 
+        System.out.println(s.printSum(2.5f, 5.5f));
+    }
+    
+}
