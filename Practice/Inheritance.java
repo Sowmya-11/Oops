ff1b98c1ddc3a4bd5a1949a519998bdd3cf811cc
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-01-26 22:30:57 +0530

    Inheritance Example

---
+++ Practice/Inheritance.java
@@ -0,0 +1,38 @@
+package Oops.Practice;
+
+class Shape {
+    String color;
+    public void area() {
+        System.out.println("displays area");
+    }
+}
+
+// Single Level Inheritance 
+class Triangle extends Shape {
+    public void area(int l, int h) {
+        System.out.println(1/2*l*h);
+    }
+}
+
+// Multi Level Inheritance 
+class EquilateralTriangle extends Triangle {
+    public void area(int l, int h) {
+        System.out.println(1/2*l*h);
+    }
+}
+
+// Hierarchial Inheritance 
+class Circle extends Shape {
+    public void area(int r) {
+        System.out.println((3.14)*r*r);
+    }
+}
+
+// Hybrid Inheritance 
+public class Inheritance {
+    public static void main(String[] args) {
+        Triangle t1=new Triangle();
+        t1.color="Red";
+    }
+    
+}
