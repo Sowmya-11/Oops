f55107214691305c206372130e878b98199e3974
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-01-28 23:46:16 +0530

    Calculating Area

---
+++ Coding/InheritanceConcept2.java
@@ -0,0 +1,18 @@
+package Coding;
+
+// Write a Java program to create a class called Shape with a method called getArea().
+// Create a subclass called Rectangle that overrides the getArea() method to calculate the area of a rectangle.
+
+class Shape {
+    void getArea() {
+        System.out.println("Calculating area");
+    }
+}
+class Rectangle extends Shape {
+    void getArea() {
+        System.out.println("Area of Rectangle");
+    }
+}
+public class InheritanceConcept2 {
+    
+}
