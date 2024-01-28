16e363213212a9ccc39560a3d949f1a1105b3733
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-01-28 23:34:11 +0530

    Adding class and methods

--- Coding/InheritanceConcept.java
+++ Coding/InheritanceConcept.java
@@ -1,8 +1,26 @@
 package Coding;
 
+// Write a Java program to create a class called Animal with a method called makeSound().
+// Create a subclass called Cat that overrides the makeSound() method to bark.
+class Animal {
+    void makeSound() {
+        System.out.println("This animal makes Sound");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("The cat quarrels");
+    }
+}
+
 public class InheritanceConcept {
     public static void main(String[] args) {
-        
+        Animal animal=new Animal();
+        animal.makeSound();
+        Cat cat=new Cat();
+        cat.makeSound();
     }
     
 }
