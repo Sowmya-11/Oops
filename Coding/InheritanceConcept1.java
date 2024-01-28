eb5776c7d5606c9ba69dd1ed7e32e7e2c0f97b77
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-01-28 23:40:14 +0530

    Adding Vehicle class

---
+++ Coding/InheritanceConcept1.java
@@ -0,0 +1,26 @@
+package Coding;
+
+// Write a Java program to create a class called Vehicle with a method called drive(). 
+// Create a subclass called Car that overrides the drive() method to print "Repairing a car".
+
+class Vehicle {
+    void drive() {
+        System.out.println("Repairing a vehicle");
+    }
+}
+class Car extends Vehicle {
+    @Override
+    void drive() {
+        System.out.println("Repairing a car");
+    }
+}
+
+public class InheritanceConcept1 {
+    public static void main(String[] args) {
+        Vehicle vehicle=new Vehicle();
+        vehicle.drive();
+        Car car=new Car();
+        car.drive();
+    }
+    
+}
