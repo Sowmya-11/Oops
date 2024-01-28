f76dbd57297ad10283b5feff84cbb2302565ca3d
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-01-28 23:52:53 +0530

    Area

--- Coding/InheritanceConcept2.java
+++ Coding/InheritanceConcept2.java
@@ -4,16 +4,28 @@ package Coding;
 // Create a subclass called Rectangle that overrides the getArea() method to calculate the area of a rectangle.
 
 class Shape {
-    void getArea() {
-        System.out.println("Calculating area");
+    public double getArea() {
+        return 0.0;
     }
 }
 class Rectangle extends Shape {
+    private double length;
+    private double width;
+    
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
     @Override
-    void getArea() {
-        System.out.println("Area of Rectangle");
+    public double getArea() {
+        return length*width;
     }
 }
+
 public class InheritanceConcept2 {
-    
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(3.0, 10.0);
+        double area = rectangle.getArea();
+        System.out.println("Area of Rectangle is "+area);
+    }
 }
