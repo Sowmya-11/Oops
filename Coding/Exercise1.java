e7331a6922b70dbb0e3b924728ef71578aad06b9
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-01-29 23:36:20 +0530

    Person program

---
+++ Coding/Exercise1.java
@@ -0,0 +1,30 @@
+package Coding;
+
+// Write a Java program to create a class called "Person" with a name and age attribute.
+// Create two instances of the "Person" class, set their attributes using the constructor, 
+// and print their name and age
+
+class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.name=name;
+        this.age=age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+}
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Person person1=new Person("John", 30);
+        Person person2=new Person("Tina", 27);
+        System.out.println(person1.getName()+" is "+person1.getAge()+" years old.");
+        System.out.println(person2.getName()+" is "+person2.getAge()+" years old.");
+    }
+    
+}
