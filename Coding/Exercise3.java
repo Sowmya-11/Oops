acbd8e085afe31ac9a4f436f644c43b4f95725f0
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-01-29 23:40:49 +0530

    Exercise3

---
+++ Coding/Exercise3.java
@@ -0,0 +1,10 @@
+package Coding;
+
+// Write a Java program to create a class called "Rectangle" with 
+// width and height attributes. Calculate the area and perimeter of the rectangle.
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        
+    }
+}
