98403e5a497cec9e4d2ac2bdec85d45cbc6614ad
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-01-31 13:56:24 +0530

    Rectangle area and perimeter

--- Coding/Exercise3.java
+++ Coding/Exercise3.java
@@ -2,9 +2,41 @@ package Coding;
 
 // Write a Java program to create a class called "Rectangle" with 
 // width and height attributes. Calculate the area and perimeter of the rectangle.
-
+class Rectangle {
+    private double width;
+    private double height;
+    public Rectangle(double width, double height) {
+        this.width=width;
+        this.height=height;
+    }
+    public double getWidth() {
+        return width;
+    }
+    public void setWidth(double width) {
+        this.width=width;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height=height;
+    }
+    public double getArea() {
+        return width*height;
+    }
+    public double getPerimeter() {
+        return (2*(width+height));
+    }
+    
+}
 public class Exercise3 {
     public static void main(String[] args) {
-        
+        Rectangle rectangle=new Rectangle(6, 3);
+        System.out.println("Area of rectangle: "+rectangle.getArea());
+        System.out.println("Perimeter of a rectangle: "+rectangle.getPerimeter());
+        rectangle.setWidth(7);
+        rectangle.setHeight(4);
+        System.out.println("Area of rectangle is now: "+rectangle.getArea());
+        System.out.println("Perimeter of a rectangle is now: "+rectangle.getPerimeter());
     }
 }
--- Coding/Exercise4.java
+++ Coding/Exercise4.java
@@ -2,8 +2,4 @@ package Coding;
 
 // Write a Java program to create a class called "Circle" with a radius attribute. 
 // You can access and modify this attribute. Calculate the area and circumference of the circle.
-public class Exercise4 {
-    public static void main(String[] args) {
-        
-    }
-}
+
