eb641b9b49f819a84a5ac80d54db2a4fea80a6a6
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-02-02 14:02:20 +0530

    Book modified

--- Coding/Exercise5.java
+++ Coding/Exercise5.java
@@ -5,10 +5,12 @@ class Book {
     String title;
     String author;
     String ISBN;
-    public Book(String title, String author, String ISBN) {
+    int pages;
+    public Book(String title, String author, String ISBN, int pages) {
         this.title=title;
         this.author=author;
         this.ISBN=ISBN;
+        this.pages=pages;
     }
     public String getTitle() {
         return title;
@@ -28,8 +30,22 @@ class Book {
     public void setISBN(String ISBN) {
         this.ISBN=ISBN;
     }
-
+    public int getPages() {
+        return pages;
+    }
+    public void setPages(int pages) {
+        this.pages=pages;
+    }
+    public void printBookDetails() {
+        System.out.println("Title: "+title);
+        System.out.println("Author: "+author);
+        System.out.println("ISBN: "+ISBN);
+        System.out.println("Pages: "+pages);
+    }
 }
 public class Exercise5 {
-    
+    public static void main(String[] args) {
+        Book book=new Book("Good", "Alex", "Not good", 200);
+        book.printBookDetails();
+    }
 }
