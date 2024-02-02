652ea52a2d9937d44207d8d420cf8e62d518b732
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-02-02 15:17:03 +0530

    Employee salary

--- Coding/Exercise6.java
+++ Coding/Exercise6.java
@@ -1,11 +1,11 @@
 package Coding;
 // Write a Java program to create a class called "Employee" with a name,
 // job title, and salary attributes, and methods to calculate and update salary.
-class Employee {
-    private String name;
-    private String jobTitle;
-    private double salary;
-    public Employee(String name, String jobTitle, double salary) {
+class Employee1 {
+    String name;
+    String jobTitle;
+    double salary;
+    public Employee1(String name, String jobTitle, double salary) {
         this.name=name;
         this.jobTitle=jobTitle;
         this.salary=salary;
@@ -27,7 +27,7 @@ class Employee {
     }
     public void setSalary(double salary) {
         this.salary=salary;
-    }  
+    }
     public void raiseSalary(double percentage) {
         salary=salary+salary*percentage/100;
     }
@@ -36,12 +36,14 @@ class Employee {
         System.out.println("JobTitle: "+jobTitle);
         System.out.println("Salary: "+salary);
     }
+
 }
 public class Exercise6 {
     public static void main(String[] args) {
-        Employee employee=new Employee("Alex", "Manager", 100000);
-        System.out.println("Employee Details");
-        employee.printEmployeeDetails();
+        Employee1 employee1=new Employee1("Alex", "Manager", 700000);
+        employee1.printEmployeeDetails();
+        employee1.raiseSalary(5);
+        System.out.println("After raising salary: ");
+        employee1.printEmployeeDetails();
     }
-    
 }
