6335e3e860d360b75ae8df60bb22467df316206f
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-02-03 18:28:56 +0530

    Sum

---
+++ Coding/BasicsI2.java
@@ -0,0 +1,9 @@
+package Coding;
+// Write a Java program to print the sum of two numbers.
+
+public class BasicsI2 {
+    public static void main(String[] args) {
+        int a=6,b=8;
+        System.out.println("Sum of 2 numbers is: "+(a+b));
+    }
+}
