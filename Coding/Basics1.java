ba2c18df0dcf38b7c858e829411641908e98d128
Sowmya-11 <sowmyareddyk375@gmail.com>
2024-02-03 18:20:50 +0530

    Print name

---
+++ Coding/Basics1.java
@@ -0,0 +1,10 @@
+package Coding;
+// Write a Java program to print 'Hello' on screen and your name on a separate line.
+
+public class Basics1 {
+    public static void main(String[] args) {
+        // System.out.println("Hello");
+        // System.out.println("Jhansi Rani Lakshmi Bhai");
+        System.out.println("Hello\nJhansi Rani Lakshmi Bhai");
+    }
+}
